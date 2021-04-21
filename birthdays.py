355e33355699913811ed025eb5f9f10843a173ac
Annanardo <annanardo00@gmail.com>
2021-04-21 22:04:56 +0200

    modified

--- birthdays.py
+++ birthdays.py
@@ -3,7 +3,7 @@ birthdays = {
     'Benjamin Franklin': '01/17/1706',
     'Ada Lovelace': '12/10/1815',
     'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+    'Rowan Atkinson': '01/6/1955', 'Anna Nardo':'05/8/2000'}
 
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
